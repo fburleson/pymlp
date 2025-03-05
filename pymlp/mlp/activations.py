fb2417c5c70edd80c8dffe16a08a4f1ff3b922a8
Joel Burleson <joelburleson0104@gmail.com>
2025-03-05 19:33:34 +0100

    removed useless functions

--- pymlp/mlp/activations.py
+++ pymlp/mlp/activations.py
@@ -9,14 +9,6 @@ def sigmoid(Z: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-Z))
 
 
-def relu(Z: np.ndarray) -> np.ndarray:
-    return np.maximum(0, Z)
-
-
-def tanh(Z: np.ndarray) -> np.ndarray:
-    pass
-
-
 def softmax(Z: np.ndarray) -> np.ndarray:
     exp_Z: np.ndarray = np.exp(Z - np.max(Z, axis=1, keepdims=True))
     return exp_Z / np.sum(exp_Z, axis=1, keepdims=True)
